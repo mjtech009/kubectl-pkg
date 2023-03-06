dd7209cbf1c4211b7b372dadf1dd63f652f11d0e
mjtech009 <113666954+mjtech009@users.noreply.github.com>
2023-03-06 16:02:43 +0530

    Create kubectl.sh

---
+++ kubectl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
+source ~/.bashrc
+sudo echo >> deb [trusted=yes] http://ftp.de.debian.org/debian buster main
+sudo apt-get update -y
+sudo apt install kubectx -y
